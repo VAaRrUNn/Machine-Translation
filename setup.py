fab731220efae716341daedd1299fc0f6e746a97
VaruN-dev-dev <sanatoo.varun666@gmail.com>
2024-01-02 08:35:48 +0530

    Done with project setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from typing import List
 with open("README.md", 'r', encoding='utf-8') as f:
     long_description = f.read()
 
@@ -7,6 +8,23 @@ REPO_NAME = "Machine-Translation"
 AUTHOR_USER_NAME = "VaruN-dev-dev"
 SRC_REPO = "machineTranslation"
 AUTHOR_EMAIL = "sanatoo.varun666@gmail.com"
+
+
+
+HYPEN_E = '-e .'
+def get_requirements(file_path: str) -> List:
+    """
+    This function will return the list of requirements, 
+    and ignore the -e .
+    """
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n", '') for req in requirements]
+
+        if HYPEN_E in requirements:
+            requirements.remove(HYPEN_E)
+    return requirements
 
 
 setuptools.setup(
@@ -19,5 +37,6 @@ setuptools.setup(
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     # project_urls =
     package_dir={"": "machineTranslation"},
-    packages=setuptools.find_packages(where="machineTranslation")
+    packages=setuptools.find_packages(where="machineTranslation"),
+    install_requires = get_requirements("requirements.txt"),
 )
